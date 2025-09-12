ab691c40422689a12afa6e8232a61f31b53b989e
livebloggerofficial <livebloggerofficial@gmail.com>
2025-09-13 02:28:41 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Pricing from "@/components/Pricing";
+import Programs from "@/components/Programs";
+import Testimonials from "@/components/Testimonials";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <Programs />
+      <WhyChooseUs />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </main>
+  );
+}
