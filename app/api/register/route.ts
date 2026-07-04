ea4893a1a41b1976a27c6fad597080db684bfdf8
Treevor <107287634+TreevorXD@users.noreply.github.com>
2026-07-03 23:42:25 -0700

    .

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,9 +1,10 @@
+import { NextRequest } from 'next/server';
 import { RegistrationEmailTemplate } from '../../../components/registration-email-template';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request) {
+export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const { contact, athletes, signature } = body;
--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,9 +1,10 @@
+import { NextRequest } from 'next/server';
 import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request) {
+export async function POST(request: NextRequest) {
   try {
     const { name, email, message } = await request.json();
 
